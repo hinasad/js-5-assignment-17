125542d3f15f75b51af2e5476bc990e2a9268005
Hina Asad <54884085+hinasad@users.noreply.github.com>
2022-11-19 21:03:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// 1. Write a program using a for loop and print the numbers from 200 till 0 with the
+// decrement of 3.
+
+for (let i = 200; i >  0; i-=3) {  
+    console.log(i);    
+}
+
+// 2. Write a program to display your name 10 times using a while loop.
+
+let i = 0;
+while (i < 10) { 
+  i++;  
+ console.log(i + ' Hina Asad ')
+}
+
+// 3. Write a program to print a sum of even numbers in between 1-10 using a while loop.
+
+let sum = 0;
+while (sum < 10) {  
+ console.log(sum);
+ sum+=2;
+}
+
+// 4. Write a program to display a number between 1 to 5 using the do while loop.
+
+let n = '';
+let j = 0;
+do {
+    j += 1; 
+    n += j ;   
+     
+} while (j < 5);
+
+console.log(n)
+
+// 5. Write a function to print the sum of two numbers.
+
+ function add(a,b) {      
+}
+ console.log(5*7);
+
+// 6. Invoke a function and pass values to the ‘width’ &amp; ‘height’ parameters inside the
+// parenthesis store the return value in a variable ‘Area’ and print it in the console.
+
+function box(width,height) {
+   return width,height;
+}
+ console.log('width ' + 40 + ' = ' + 'height ' + 50)
